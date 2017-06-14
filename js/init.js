a30582854f69186ae7438d212606ee6405af5842
tkoar <terrancekoar@gmail.com>
2017-06-14 15:48:29 -0400

    Done.

--- js/init.js
+++ js/init.js
@@ -1,10 +1,12 @@
-jQuery(document).ready(function($) {
-  var API_KEY = "GO GET AN API KEY";
+$(document).ready(function($) {
+  var API_KEY = "ab5978e6495974d0";
   var URL = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
   var ctx = $("#NYCWeatherChart").get(0).getContext("2d");
 
   makeAjaxRequest(URL, function(json) {
     var data = generateDataSet(getHours(json), getFahrenheits(json));
     var tempChart = new Chart(ctx).Line(data, { bezierCurve: true });
-  });
-});
+    // document.getElementById("NYCWeatherChart").insertAdjacentHTML('beforeend', tempChart)
+  })
+
+})
--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,46 @@
-function getFahrenheits(result){
-  // Your code goes here
+function getFahrenheits(result) {
+  var tempArr = []
+  var arr = result.hourly_forecast
+  for (var i = 0; i < arr.length; i++) {
+    tempArr.push(arr[i].temp.english)
+  }
+  return tempArr
 }
 
-function getHours(result){
-  // Your code goes here
+function getHours(result) {
+  var timeArr = []
+  var arr = result.hourly_forecast
+  for (var i = 0; i < arr.length; i++) {
+    timeArr.push(arr[i].FCTTIME.hour)
+  }
+  return timeArr
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+  return {
+    labels: labels,
+  datasets: [
+    {
+      label: "Hourly Weather for New York",
+      fillColor: "rgba(220,220,220,0.2)",
+      strokeColor: "rgba(220,220,220,1)",
+      pointColor: "rgba(220,220,220,1)",
+      pointStrokeColor: "#fff",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "rgba(220,220,220,1)",
+      data: data
+    }
+    ]
+  }
 }
 
 function makeAjaxRequest(endpoint, success) {
-  // Your code goes here
+  $.ajax({
+    url: endpoint,
+    dataType: 'jsonp',
+    success: success
+    // function (dataResult) {
+    //   return dataResult.hourly_forecast
+    // }
+  })
 }
